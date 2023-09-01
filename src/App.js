699028c313bd0b7c5aa87963bfb7c193f857f21d
Desmond <desmondmpofu@gmail.com>
2023-09-01 19:08:36 +0200

    children props

--- src/App.js
+++ src/App.js
@@ -28,13 +28,23 @@ function App() {
      </div>
      <p className="message">Step {step}: {messages[step-1]}</p>
      <div className="buttons">
-        <button style={{backgroundColor:'#7950f2', color:'#fff'}} onClick= {handlePrevious}>Previous</button>
-        <button style={{backgroundColor:'#7950f2', color:'#fff'}} onClick={handleNext}>Next</button>
+        <Button textColor='#fff' bgColor='#7950f2' text='Previous' onClick= {handlePrevious} ><span>👈</span>Previous</Button>
+        <Button textColor='#fff' bgColor='#7950f2' text='Next' onClick={handleNext}>Next<span>👉</span></Button>
      </div>
     </div>
     }
     </>
   )
+}
+
+function Button({textColor, bgColor, onClick, children}){
+    return(
+        <button
+        style={{backgroundColor:bgColor, color:textColor}} onClick={onClick}>{children}</button>
+
+        
+    )
+
 }
 
 export default App;
